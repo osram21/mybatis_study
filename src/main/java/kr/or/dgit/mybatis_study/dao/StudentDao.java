8bb490ca0d4aa9bf8d95f5e54f2bc5360cfed8ad
osram21 <osram21@naver.com>
2017-03-09 12:31:36 +0900

    Test

--- src/main/java/kr/or/dgit/mybatis_study/dao/StudentDao.java
+++ src/main/java/kr/or/dgit/mybatis_study/dao/StudentDao.java
@@ -7,4 +7,7 @@ import kr.or.dgit.mybatis_study.dto.Student;
 public interface StudentDao {
 	int insertStudent(Student student);
 	List<Student> selectStudentByAll();
+	Student selectStudentByNo(int studNo);
+/*	int updateStudent(Student student);
+	int deleteStudentByNo(int studNo);*/
 }
